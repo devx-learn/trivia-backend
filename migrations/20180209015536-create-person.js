c996780b788248c72c1d05a566df5bcc20e52c53
KateKassab <katertot21@gmail.com>
2018-02-09 11:12:51 -0800

    updated with db and seeds

--- migrations/20180209015536-create-person.js
+++ migrations/20180209015536-create-person.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('People', {
+    return queryInterface.createTable('Person', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('People');
+    return queryInterface.dropTable('Person');
   }
 };
