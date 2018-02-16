984537903effee154be2d6cebfc535ab84b0be4a
Eric Gelencser <Ericgelencsermusic@hotmail.com>
2018-02-15 21:42:30 -0800

    ready for depoy

--- socket-app/connections/trivia/index.js
+++ socket-app/connections/trivia/index.js
@@ -1,6 +1,8 @@
 const SocketApp = require('../../socket-app');
 const ANSWER_TIME = 30000; // 30 Seconds
-
+const request = require('request');
+const START_TIME = 15000;
+const MAX_PLAYERS = 10
 /**
  * Shuffles array in place.
  * @param {Array} a items An array containing the items.
@@ -15,83 +17,118 @@ function shuffle(a) {
   }
   return a;
 }
-
 class Game {
   constructor(opts) {
-    this.socket = opts.socket;
-    this.username = opts.username;
-    this.questions = opts.questions;
-    this.questionIndex = 0;
-    this.score = 0;
+    console.log("strting new game");
+    this.players = opts.players;
+    this.room = opts.room;
     this.anwerTimer = null;
     this.nextQuestionTimer = null;
     this.sendQuestion();
   }
+  getNextQuestion(cb) {
+    request.get('https://opentdb.com/api.php?amount=1', (err, response) => {
+      if (err) return cb(err)
+      let body = {};
+      try {
+        body = JSON.parse(response.body);
+      } catch (ex) {
+        return cb(ex);
+      }
+      if (body.results.length >= 0) return cb(null, body.results[0])
+      else cb('No question returned')
+    });
+  }
 
   sendQuestion() {
-    this.nextQuestionTimer = setTimeout(() => { //once the setTimeout function has started, exicute "sendQuestion"
-      this.currentQuestion = this.questions[this.questionIndex]; //pulls from questions.json
-      this.currentQuestion.points = ['', 'easy', 'medium', 'hard'].indexOf(this.currentQuestion.difficulty) * 5; //caculates points based off the difficulty of the question by multiplying by the index of an array of 1-3 not including 0
-      let answers = shuffle([this.currentQuestion.correct_answer].concat(this.currentQuestion.incorrect_answers)); //shuffles the correct and incorrect answers and combinds them into a new array.
-      this.socket.emit('question', { //e
-        question: this.currentQuestion.question,
-        category: this.currentQuestion.category,
-        points: this.currentQuestion.points,
-        answers: answers
-      });
-      this.questionIndex += 1;
-      this.questionIndex = this.questionIndex % this.questions.length;
-
-      this.answerTimer = setTimeout(() => {
-        this.socket.removeAllListeners('answer');
-        this.socket.emit('timeout', this.currentQuestion.correct_answer);
-        this.sendQuestion();
-      }, 30000)
-
-      this.socket.on('answer', this.checkAnswer.bind(this));
+    let _this = this;
+    let playerList = this.players.map(player =>{
+      return {
+        username: player.username,
+        score: player.score
+      }
+    })
+    this.players.forEach(player =>{
+      player.socket.emit("player-list", playerList)
+    })
+    this.answerCount = 0;
+    this.nextQuestionTimer = setTimeout(() => {
+      _this.getNextQuestion((err, question) => {
+        if (err) return console.log('Error getting question' + err);
+        _this.currentQuestion = question;
+        _this.currentQuestion.points = ['', 'easy', 'medium', 'hard'].indexOf(_this.currentQuestion.difficulty) * 5;
+        let answers = shuffle([_this.currentQuestion.correct_answer].concat(_this.currentQuestion.incorrect_answers));
+        console.log("sending question");
+        _this.room.emit('question', {
+          question: _this.currentQuestion.question,
+          category: _this.currentQuestion.category,
+          points: _this.currentQuestion.points,
+          answers: answers
+        });
+        _this.players.forEach((player) => {
+          player.socket.on('answer', _this.checkAnswer.bind(_this, player));
+        })
+      })
     }, 2000)
   }
-
-  checkAnswer(answer) {
-    clearTimeout(this.answerTimer);
-    this.socket.removeAllListeners('answer');
+  checkAnswer(player, answer) {
+    this.answerCount++;
+    player.socket.removeAllListeners('answer');
     if (answer === this.currentQuestion.correct_answer) {
-      this.score += this.currentQuestion.points;
-      this.socket.emit('right', this.score);
+      player.score += this.currentQuestion.points;
+      player.socket.emit('right', player.score);
     } else {
-      this.socket.emit('wrong', this.currentQuestion.correct_answer);
+      player.socket.emit('wrong', this.currentQuestion.correct_answer);
     }
-    this.sendQuestion();
+    if (this.answerCount >= this.players.length) {
+      this.sendQuestion();
+    }
   }
-
   stop() {
     clearTimeout(this.answerTimer);
     clearTimeout(this.nextQuestionTimer);
   }
 }
-
 class Trivia extends SocketApp {
   constructor(io, connectionName) {
     super(io, connectionName);
-    this.questions = require('./questions').results; // will be connected to the fetch of the API
-    this.games = {}; // initializes and empty object of games
+    this.io = io;
+    this.games = []; // creates an empty array of game rooms
+    this.gameId = 0; // creates a UUI for the game room
+    this.players = [] // creates an empty array of playes
   }
-
   onConnect(socket) {
-    socket.on('signin', (username) => { //waits for a sign-in even from the front-end
-      this.games[socket.id] = new Game({ //
-        username: username,
-        socket: socket,
-        questions: shuffle(this.questions)
-      });
-    })
-
-    socket.on('disconnect', () => {
-      this.games[socket.id].stop();
-      delete this.games[socket.id];
-    })
+    let _this = this;
+    if (this.players.length < MAX_PLAYERS) {
+      socket.on('signin', (username) => {
+        _this.players.push({
+          username: username,
+          socket: socket,
+          score: 0
+        })
+        let roomId = 'game-' + _this.gameId;
+        socket.join(roomId, () => {
+          socket.emit('waiting')
+          if (this.players.length === 1){
+            _this.startTimer = setTimeout(()=>{
+              _this.games.push(new Game({
+                room: _this.server.in(roomId),
+                players: _this.players,
+              }))
+            },START_TIME)
+          }
+          if (_this.players.length === MAX_PLAYERS) {
+            clearTimeout(_this.startTimer);
+            _this.games.push(new Game({
+              room: _this.server.in(roomId),
+              players: _this.players,
+            }))
+            _this.gameId++;
+          }
+        }) //makes a unique id for game
+      })
+    }
+    socket.on('disconnect', () => { })
   }
 }
-
-
 module.exports = Trivia;
